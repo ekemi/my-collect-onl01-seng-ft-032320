9185756bf2b3574d08ea020ed80889bb451dbc02
edmundo papi <edmundopierre@yahoo.com>
2020-03-27 21:22:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,4 @@
-# def my_collect(array)
-#   counter = 0
-#
-#   while counter < array.length
-#     yield array[counter]
-#     counter +=1
-#   end
-#
-# end
+
 def my_collect(array)
     collection =[]
     counter = 0
